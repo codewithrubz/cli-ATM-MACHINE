05af4d740d60a3f5147d468dbc645a7f533082a8
RameenKhan <rameenkhan8787@gamil.com>
2024-03-29 21:51:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPinCode = 12345;
+let pinAns = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number",
+    },
+]);
+if (pinAns.pin === myPinCode) {
+    console.log("Correct pincode!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "select your operation",
+            type: "list",
+            choices: ["withdraw", "checkbalance", "passedcash"],
+        },
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            },
+        ]);
+        myBalance -= amountAns.amount;
+        if (amountAns.amount <= 10000) {
+            console.log(`your remaining amount is ${myBalance}$`);
+        }
+        else {
+            console.log("Please enter a valid amount you have only 10000");
+        }
+    }
+    else if (operationAns.operation === "checkbalance") {
+        console.log(`your balance is:${myBalance}$`);
+    }
+    else if (operationAns.operation === "passedcash") {
+        let cashAns = await inquirer.prompt([
+            { name: "cash",
+                message: "which amount you want to passedcash",
+                type: "list",
+                choices: [5000, 10000, 15000, 20000],
+            }
+        ]);
+        console.log(`today you passed ${cashAns.cash} cash in your bank account so now your total amount is ${myBalance + cashAns.cash}`);
+    }
+}
